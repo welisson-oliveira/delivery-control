dfcc59c174f6342f02a272d322a1737f5e0f2f8c
Welisson Oliveira <welisson.oliveiraa@gmail.com>
2023-08-04 18:10:37 -0300

    Commit inicial

---
+++ src/main/java/com/acert/deliverycontrol/DeliveryControlApplication.java
@@ -0,0 +1,13 @@
+package com.acert.deliverycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acert/deliverycontrol/DeliveryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acert.deliverycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
